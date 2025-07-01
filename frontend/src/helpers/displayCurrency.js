a984d45326c1be81a80ce33121d7c94e4ffdddef
kavindupbsr <bkpabasara@gmail.com>
2025-07-01 20:03:35 +0530

    feat: add helper functions for cart management, currency display, category fetching, product categories, and scroll functionality

---
+++ frontend/src/helpers/displayCurrency.js
@@ -0,0 +1,12 @@
+const displayLKRCurrency = (num) => {
+    const formatter = new Intl.NumberFormat('en-LK',{
+        style : "currency",
+        currency : 'LKR',
+        minimumFractionDigits : 2
+    })
+
+    return formatter.format(num)
+
+}
+
+export default displayLKRCurrency
